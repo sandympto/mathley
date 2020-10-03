bea59e1668fbd56fb1eb1033c8b6e4f3a9342f35
Sandeep Tayal <sandympto@gmail.com>
2020-10-04 01:09:32 +0530

    DSP: arctan.c : modified to take the PI/4 value correct

--- dsp/flp/arctan.c
+++ dsp/flp/arctan.c
@@ -10,25 +10,25 @@
 
 float arctan(float x)
 {
-    int flag = 0;	
+    int flag = 0; 
+    float val = 0;
     if (x < 0)
     {
         x = -x;
 	flag = 1;
     }
-    float val = 0;
     if (abs(x) < 1)
     {	    
         val = x - (x*x*x)/3 + (x*x*x*x*x)/5 
-	    - (x*x*x*x*x*x*x)/7;
+	    - (x*x*x*x*x*x*x)/7 + (x*x*x*x*x*x*x*x*x)/9 - (x*x*x*x*x*x*x*x*x*x*x)/11
+	    + (x*x*x*x*x*x*x*x*x*x*x*x*x)/13;
 	if (flag == 1)
 	{
 	    val = -val;
 	}
         return val;
     }
-    //arctan (1/x) = 0.5 * pi - arctan(x) [x > 0]
-    if (x >= 1)
+    if (x > 1)
     {
         val = 3.141926/2 - arctan(1/x);
 	if (flag == 1)
@@ -37,13 +37,29 @@ float arctan(float x)
 	}
         return val;	
     }
+    if (abs(x - 1) < 0.001)
+    {
+	val = 3.141926/4;
+	if (flag == 1)
+	{
+	    val = -val;
+	}
+	printf(" hit %f", x);
+	return val;
+    }
 }
 
 
 int main()
 {   
-    float f = -123535;
+    float f = -1;
     float val = arctan(f);
-    printf("val = %f", val);
+    while(f < 1)
+    {
+      val = arctan(f);
+      if (abs(val) > 0.001);
+      printf("f = %f %f \n", atan(f), val);
+      f = f + .01;
+    }
     return 0;
 }
