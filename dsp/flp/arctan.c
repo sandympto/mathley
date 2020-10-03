dd0b84966197d60ee4d7a22c7fa8d6e9ed3d0e4e
Sandeep Tayal <sandympto@gmail.com>
2020-10-03 20:24:52 +0530

    DSP: arctan.c : modified to include complete range of arctan

--- dsp/flp/arctan.c
+++ dsp/flp/arctan.c
@@ -2,7 +2,6 @@
  * filename : arctan.c
  * author   : Sandeep Tayal
  * email    : sandympto@gmail.com
- * note     : this function works only when |x| < 1 
  *****************************************************************************/
 
 #include <stdio.h>
@@ -11,19 +10,39 @@
 
 float arctan(float x)
 {
+    int flag = 0;	
+    if (x < 0)
+    {
+        x = -x;
+	flag = 1;
+    }
     float val = 0;
     if (abs(x) < 1)
     {	    
         val = x - (x*x*x)/3 + (x*x*x*x*x)/5 
 	    - (x*x*x*x*x*x*x)/7;
+	if (flag == 1)
+	{
+	    val = -val;
+	}
         return val;
+    }
+    //arctan (1/x) = 0.5 * pi - arctan(x) [x > 0]
+    if (x >= 1)
+    {
+        val = 3.141926/2 - arctan(1/x);
+	if (flag == 1)
+	{
+	    val = -val;
+	}
+        return val;	
     }
 }
 
 
 int main()
 {   
-    float f = 0.789;
+    float f = -123535;
     float val = arctan(f);
     printf("val = %f", val);
     return 0;
