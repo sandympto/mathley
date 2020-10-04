664c8457542dd4481df45e90946189079e2eaa8b
Sandeep Tayal <sandympto@gmail.com>
2020-10-04 16:57:00 +0530

    DSP: arctan.c: corrected white space

--- dsp/flp/arctan.c
+++ dsp/flp/arctan.c
@@ -15,47 +15,47 @@ float arctan(float x)
     if (x < 0)
     {
         x = -x;
-	flag = 1;
+        flag = 1;
     }
     if (fabs(x) <= 0.5)
-    {	    
+    {    
         val = x - (x*x*x)/3 + (x*x*x*x*x)/5 
-	    - (x*x*x*x*x*x*x)/7 + (x*x*x*x*x*x*x*x*x)/9 - (x*x*x*x*x*x*x*x*x*x*x)/11
-	    + (x*x*x*x*x*x*x*x*x*x*x*x*x)/13;
-	if (flag == 1)
-	{
-	    val = -val;
-	}
+                - (x*x*x*x*x*x*x)/7 + (x*x*x*x*x*x*x*x*x)/9 - (x*x*x*x*x*x*x*x*x*x*x)/11
+                + (x*x*x*x*x*x*x*x*x*x*x*x*x)/13;
+        if (flag == 1)
+        {
+            val = -val;
+        }
         return val;
     }
     if ((fabs(x) > 0.5) && (fabs(x)<1))
-    {	    
+    {    
         //arctan (x) = arctan(c) + arctan((x - c) / (1 + x*c))	//
-	val  = arctan(0.5) + arctan((x - 0.5)/ (1 + x * 0.5));    
-	if (flag == 1)
-	{
-	    val = -val;
-	}
+        val  = arctan(0.5) + arctan((x - 0.5)/ (1 + x * 0.5));    
+        if (flag == 1)
+        {
+            val = -val;
+        }
         return val;
     }
 
     if (x > 1)
     {
         val = 3.141926/2 - arctan(1/x);
-	if (flag == 1)
-	{
-	    val = -val;
-	}
-        return val;	
+        if (flag == 1)
+        {
+            val = -val;
+        }
+        return val;
     }
     if (fabs(x - 1) < 0.001)
     {
-	val = 3.141926/4;
+        val = 3.141926/4;
         if (flag == 1)
-	{
+        {
             val = -val;
         }
-	return val;
+        return val;
     }
 }
 
